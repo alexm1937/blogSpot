8414de0f6f39bc272e6e3b0a2d1f1ef5e230f353
unknown <alexm1397@gmail.com>
2021-11-26 20:40:52 -0600

    created user model

---
+++ models/User.js
@@ -0,0 +1,58 @@
+
+const bcrypt = require('bcrypt'); //pass hashing
+const { Model, Datatypes } = require('sequelize'); //sequelize model class
+const sequelize = require('../config/connection');
+
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
+User.init(
+    //table columns
+    {
+        id: {
+            type: Datatypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {isEmail: true}
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: { len: [4] }
+        }
+    },
+    //hooks and table config 
+    {
+        hooks: {
+            //beforeCreate lifecycle hook hashes password on user create(before user is created!)
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData
+            }
+        },
+        //table config
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user'
+    }
+);
+
+module.exports = User;
