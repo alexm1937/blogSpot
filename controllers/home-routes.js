a433961077b76851f6b2df40807e9151b2473f86
unknown <alexm1397@gmail.com>
2021-11-28 11:17:28 -0600

    created main handlebars template and route for homepage

---
+++ controllers/home-routes.js
@@ -0,0 +1,23 @@
+
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const {Post, User, Comment} = require('../models');
+
+//get homepage populated with posts
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: ['id', 'title', 'contents', 'user_id', 'created_at'],
+        order: [['created_at', 'DESC']]
+        ///include models
+    })
+    .then(dbPostData => {
+        const posts = dbPostData.map(post => post.get({plain: true}));//makes posts arr to pass to template
+        res.render('homepage', {posts, loggedIn: req.session.loggedIn});//renders main.handlebars then passes homepage into {{{body}}}
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
